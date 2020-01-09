34f2a84a745366dd9515b1b39834f676bac1ae97
LPascholatti <lucaspascholatti@gmail.com>
2020-01-09 14:49:29 +0100

    Setting up redirect function from the new shortened url code

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const port = process.env.PORT || 4000
 const app = express();
 
 app.use(express.json({ extended: false}));
-app.use('/index', require('./routes/index'));
+app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
 connectDB();
 
-app.get('/', (req, res) => res.send('Welcome to this URL Shortener'))
+// app.get('/', (req, res) => res.send('Welcome to this URL Shortener'))
 app.listen(port, () => console.log(`Example App listening on port ${port}`))
